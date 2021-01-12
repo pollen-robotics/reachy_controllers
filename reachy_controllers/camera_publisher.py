9d52f7fa7f20d257ac51a1eeeb2c872cdd315815
Simon Le Goff <simheo0@gmail.com>
2021-01-12 18:12:14 +0100

    Use two different timers for camera publishers.

--- reachy_controllers/camera_publisher.py
+++ reachy_controllers/camera_publisher.py
@@ -4,7 +4,7 @@ Camera Node.
 - publish /left_image and /right_image at the specified rate (default: 30Hz)
 
 """
-
+from functools import partial
 import rclpy
 from rclpy.node import Node
 
@@ -15,7 +15,6 @@ from cv_bridge import CvBridge
 
 class CameraPublisher(Node):
     """Camera Publisher class."""
-
     def __init__(self, size: tuple = (1280, 720), fps: float = 30.0) -> None:
         """Connect to both cameras, initialize the publishers."""
         super().__init__('camera_publisher')
@@ -40,25 +39,35 @@ class CameraPublisher(Node):
         self.cap_right.set(cv.CAP_PROP_FRAME_WIDTH, size[0])
         self.cap_right.set(cv.CAP_PROP_FRAME_HEIGHT, size[1])
 
+        self.cap = {
+            'left': self.cap_left,
+            'right': self.cap_right
+        }
+
         self.clock = self.get_clock()
         self.camera_publisher_left = self.create_publisher(CompressedImage, 'left_image', 1)
-        self.publish_timer_l = self.create_timer(timer_period_sec=1/fps, callback=self.publish_left)
+        self.publish_timer_l = self.create_timer(
+            timer_period_sec=1/fps,
+            callback=partial(self.publish_img, 'left')
+            )
         self.camera_publisher_right = self.create_publisher(CompressedImage, 'right_image', 1)
-        self.publish_timer_r = self.create_timer(timer_period_sec=1/fps, callback=self.publish_right)
+        self.publish_timer_r = self.create_timer(
+            timer_period_sec=1/fps,
+            callback=partial(self.publish_img, 'right')
+            )
+
+        self.publisher = {
+            'left': self.camera_publisher_left,
+            'right': self.camera_publisher_right
+        }
 
         self.bridge = CvBridge()
 
-    def publish_left(self) -> None:
-        """Read image from the left camera, convert it to an Image ROS msg and publishes it."""
-        _, img_left = self.cap_left.read()
-        self.img_msg_left = self.bridge.cv2_to_compressed_imgmsg(img_left)
-        self.camera_publisher_left.publish(self.img_msg_left)
-
-    def publish_right(self) -> None:
-        """Read image from the right camera, convert it to an Image ROS msg and publishes it."""
-        _, img_right = self.cap_right.read()
-        self.img_msg_right = self.bridge.cv2_to_compressed_imgmsg(img_right)
-        self.camera_publisher_right.publish(self.img_msg_right)
+    def publish_img(self, side: str) -> None:
+        """Read image from the requested side and publishes it."""
+        _, img = self.cap[side].read()
+        img_msg = self.bridge.cv2_to_compressed_imgmsg(img)
+        self.publisher[side].publish(img_msg)
 
 
 def main() -> None:
