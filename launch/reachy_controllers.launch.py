8adc4468dd107b7042c9e1ba0c1da9b456ca0af5
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-03-05 17:21:30 +0100

    Add reachy_controllers launch file.

---
+++ launch/reachy_controllers.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='reachy_controllers',
+            executable='joint_state_controller',
+        ),
+        Node(
+            package='reachy_controllers',
+            executable='camera_publisher',
+        ),
+        Node(
+            package='reachy_controllers',
+            executable='camera_zoom_service',
+        ),
+    ])
