e76bb9433feb87977ca24d0d57c7de501c9ed103
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-03-05 17:03:51 +0100

    Fix flake8.

--- reachy_controllers/zoom_service.py
+++ reachy_controllers/zoom_service.py
@@ -1,4 +1,4 @@
-'''Service node to manage zoom services.'''
+"""Service node to manage zoom for cameras."""
 import rclpy
 from rclpy.node import Node
 
@@ -8,59 +8,54 @@ from optical_zoom.zoom_piloting import ZoomController
 
 
 class ZoomControllerService(Node):
-    '''Main node creating zoom services.'''
-    def __init__(self):
+    """Main node creating the zoom services for cameras."""
+
+    def __init__(self) -> None:
+        """Set up the node and create the services."""
         super().__init__('zoom_controller_service')
+        self.logger = self.get_logger()
+
         self.controller = ZoomController()
         self.command_service = self.create_service(
             ZoomCommand,
             'zoom_command',
-            self.command_callback
+            self.zoom_command_callback
             )
+        self.logger.info(f'Launching "{self.command_service.srv_name}" service.')
+
         self.speed_service = self.create_service(
             SetZoomSpeed,
             'zoom_speed',
-            self.speed_callback
+            self.zoom_speed_callback
             )
-        self.logger = self.get_logger()
-        self.logger.info('Initialized ZoomControllerService.')
+        self.logger.info(f'Launching "{self.speed_service.srv_name}" service.')
 
-    def command_callback(self, request, response):
-        '''
-        request:
-            - string side
-            - string zoom_command
-        response:
-            None
-        '''
+        self.logger.info('Node ready!')
+
+    def zoom_command_callback(self, request: ZoomCommand.Request, response: ZoomCommand.Response) -> ZoomCommand.Response:
+        """Handle zoom_command request."""
         req = request.zoom_command
         if req == 'homing':
             self.controller.homing(request.side)
             return response
 
         if req not in ('in', 'out', 'inter'):
-            raise ValueError("Invalid command sent to zoom controller. Command must be either 'homing', 'in', 'out' or 'inter'.")
+            raise ValueError("Invalid command sent to zoom controller. Command must be in ('homing', 'in', 'out' or 'inter').")
         self.controller.send_zoom_command(request.side, request.zoom_command)
         return response
 
-    def speed_callback(self, request, response):
-        '''
-        request:
-            - int16 speed
-        response:
-            None
-        '''
+    def zoom_speed_callback(self, request: SetZoomSpeed.Request, response: SetZoomSpeed.Response) -> SetZoomSpeed.Response:
+        """Handle zoom_speed request."""
         self.controller.set_speed(request.speed)
         return response
 
 
 def main(args=None):
-    '''Main.'''
+    """Run main loop."""
     rclpy.init(args=args)
 
-    orb_kin_service = ZoomControllerService()
-
-    rclpy.spin(orb_kin_service)
+    zoom_controller_service = ZoomControllerService()
+    rclpy.spin(zoom_controller_service)
 
     rclpy.shutdown()
 
