e40df9e740006a1859ce74a5fc1c3fa0db6cbdf2
Simon Le Goff <32677536+simheo@users.noreply.github.com>
2021-06-03 16:16:07 +0200

    Update test_orbita_fan.py

--- setup/test_orbita_fan.py
+++ setup/test_orbita_fan.py
@@ -18,7 +18,7 @@ def main():
 
     r.start()
     r.set_orbita_values('fan_trigger_temperature_threshold', 'neck', {'disk_top': 15})
-    logger.info("Changing Orbita's fan trigger temperature for 5 seconds, check if you here the fan.")
+    logger.info("Changing Orbita's fan trigger temperature for 5 seconds, check if you hear the fan.")
     time.sleep(5.0)
     r.set_orbita_values('fan_trigger_temperature_threshold', 'neck', {'disk_top': 35})
     r.stop()
