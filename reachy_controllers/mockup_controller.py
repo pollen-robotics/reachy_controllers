f0a622880271d592ee81727b6275bcc3b2d9a343
Pierre Rouanet <pierre.rouanet@gmail.com>
2021-11-24 14:35:02 +0100

    Publish on forward commands.

--- reachy_controllers/mockup_controller.py
+++ reachy_controllers/mockup_controller.py
@@ -2,6 +2,7 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import JointState
+from std_msgs.msg import Float64MultiArray
 
 from reachy_msgs.msg import PidGains
 from reachy_msgs.srv import GetJointFullState
@@ -15,21 +16,9 @@ class MockupController(Node):
         self.logger = self.get_logger()
         self.clock = self.get_clock()
 
-        self.joint_state = JointState()
-        self.setup_joints()
+        self.forward_command = Float64MultiArray()
+        self.forward_command.data = [0.0 for _ in self.joint_names]
         self.id4joint = {joint: i for i, joint in enumerate(self.joint_names)}
-
-        self.joint_state_publisher = self.create_publisher(
-            msg_type=JointState,
-            topic='joint_states',
-            qos_profile=5,
-        )
-        self.joint_state.name = self.joint_names
-        self.joint_state_pub_timer = self.create_timer(
-            timer_period_sec=1/state_pub_rate,
-            callback=self.publish_joint_states,
-        )
-        self.logger.info(f'Setup "{self.joint_state_publisher.topic_name}" publisher ({state_pub_rate:.1f}Hz).')
 
         self.joint_goal_subscription = self.create_subscription(
             msg_type=JointState,
@@ -67,33 +56,26 @@ class MockupController(Node):
         )
         self.logger.info(f'Create "{self.fan_srv.srv_name}" service.')
 
-    def setup_joints(self):
-        self.joint_state.position = [0.0 for _ in self.joint_names]
-
+        self.forward_command_pub = self.create_publisher(
+            Float64MultiArray, '/forward_position_controller/commands', qos_profile=5,
+        )
 
     @property
     def joint_names(self):
         return (
             'l_shoulder_pitch', 'l_shoulder_roll', 'l_arm_yaw', 'l_elbow_pitch', 
-            'l_forearm_yaw', 'l_wrist_pitch', 'l_wrist_roll', 'l_gripper',
+            'l_forearm_yaw', 'l_wrist_pitch', 'l_wrist_roll',
             'r_shoulder_pitch', 'r_shoulder_roll', 'r_arm_yaw', 'r_elbow_pitch', 
-            'r_forearm_yaw', 'r_wrist_pitch', 'r_wrist_roll', 'r_gripper',
-            'l_antenna', 'r_antenna',
+            'r_forearm_yaw', 'r_wrist_pitch', 'r_wrist_roll',
             'orbita_roll', 'orbita_pitch', 'orbita_yaw',
+            'l_antenna', 'r_antenna',
+            'r_gripper', 'l_gripper',
         )
 
-
-    def publish_joint_states(self):
-        self.joint_state.header.stamp = self.clock.now().to_msg()
-        self.joint_state_publisher.publish(self.joint_state)
-
     def on_joint_goals(self, msg: JointState) -> None:
-        for pos, name in zip(msg.position, msg.name):
-            self.joint_state.position[self.id4joint[name]] = pos
-        for vel, name in zip(msg.velocity, msg.name):
-            self.joint_state.velocity[self.id4joint[name]] = pos
-        for eff, name in zip(msg.effort, msg.name):
-            self.joint_state.effort[self.id4joint[name]] = pos
+        for name, pos in zip(msg.name, msg.position):
+            self.forward_command.data[self.id4joint[name]] = pos
+        self.forward_command_pub.publish(self.forward_command)
 
     def get_joint_full_state(self,
                              request: GetJointFullState.Request,
@@ -101,13 +83,13 @@ class MockupController(Node):
                              ) -> GetJointFullState.Response:
         """Handle GetJointsFullState service request."""
         response.name = self.joint_names
-        response.present_position = self.joint_state.position
-        response.present_speed = self.joint_state.velocity
-        response.present_load = self.joint_state.effort
+        response.present_position = 0.0
+        response.present_speed = 0.0
+        response.present_load = 0.0
 
         response.temperature = [37.5 for _ in self.joint_names]
         response.compliant = [False for _ in self.joint_names]
-        response.goal_position = self.joint_state.position
+        response.goal_position = 0.0
         response.speed_limit = [0.0 for _ in self.joint_names]
         response.torque_limit = [100.0 for _ in self.joint_names]
         response.pid_gain = [PidGains(p=1.0, i=0.0, d=0.0) for _ in self.joint_names]
