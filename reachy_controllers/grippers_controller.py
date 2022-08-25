0ed9240538c8fda2b86005d11f3b6cd224d1c745
glannuzel <glannuzel@ensc.fr>
2022-08-25 16:56:19 +0200

    Update grippers_controller.py

--- reachy_controllers/grippers_controller.py
+++ reachy_controllers/grippers_controller.py
@@ -18,8 +18,8 @@ each dt based on the evolution of the requests
 import time
 from collections import deque
 from enum import Enum
-from threading import Thread
-from matplotlib.pyplot import cla
+from threading import Thread, Event
+from typing import Dict
 
 import numpy as np
 
@@ -42,12 +42,12 @@ P_DIRECT_CONTROL = 5.0
 
 HISTORY_LENGTH = 10
 UPDATE_FREQ = 100  # Hz
-DT = 1 // UPDATE_FREQ
+DT = 1 / UPDATE_FREQ
 MOVING_SPEED = np.deg2rad(100)
 INC_PER_DT = DT * MOVING_SPEED
 
 SKIP_EARLY_DTS = 15
-MIN_MOVING_DIST = 0.002
+MIN_MOVING_DIST = 0.004
 MAX_ERROR = 5.0 * np.pi / 180
 
 
@@ -62,64 +62,76 @@ class GripperState:
     """Represent the current gripper state."""
     def __init__(
         self,
+        name: str,
         is_direct: bool,
-        present_position: float, requested_goal_position: float,
+        present_position: float, user_requested_goal_position: float,
+        p: float = P_DIRECT_CONTROL, i: float = 0.0, d: float = 0.0,
     ) -> None:
 
+        self.name = name
         self.is_direct = is_direct
 
         self.present_position = deque([present_position], HISTORY_LENGTH)
-        self.requested_goal_position = deque([requested_goal_position], HISTORY_LENGTH)
+        self.user_requested_goal_position = deque([user_requested_goal_position], HISTORY_LENGTH)
 
-        self.interpolated_goal_position = deque([], HISTORY_LENGTH)
-        self.error = deque([], HISTORY_LENGTH)
-        self.in_collision = deque([], HISTORY_LENGTH)
+        self.interpolated_goal_position = deque([user_requested_goal_position], HISTORY_LENGTH)
+        self.error = deque([], HISTORY_LENGTH//2)
+        self.in_collision = deque([False], HISTORY_LENGTH)
+
+        self.safe_computed_goal_position = user_requested_goal_position
 
         self.elapsed_dts_since_change_of_direction = 0
         self.elapsed_dts_since_collision = 0
 
-    def update(self, new_present_position: float, new_requested_goal_position: float):
+        self.pid = p, i, d
+
+    def update(self, new_present_position: float, new_user_requested_goal_position: float):
         self.present_position.append(new_present_position)
 
-        if self.has_changed_direction(new_requested_goal_position):
+        if self.has_changed_direction(new_user_requested_goal_position):
             self.elapsed_dts_since_change_of_direction = 0
 
-        self.requested_goal_position.append(new_requested_goal_position)
+        self.user_requested_goal_position.append(new_user_requested_goal_position)
 
-        # TODO: use enum instead
         collision_state = self.check_collision_state()
 
         if collision_state == CollisionState.NO_COLLISION:
-            interpolated_goal_position = self.compute_fake_error_goal_position()
+            interpolated_goal_position = self.compute_close_smart_goal_position()
+            self.safe_computed_goal_position = new_user_requested_goal_position
 
         elif collision_state == CollisionState.ENTERING_COLLISION:
-            self.set_pid(gripper, p=P, i=0.0, d=0.0)
+            self.set_pid(p=P_SAFE_CLOSE, i=0.0, d=0.0)
             interpolated_goal_position = self.compute_fake_error_goal_position()
+            self.safe_computed_goal_position = interpolated_goal_position
 
         elif collision_state == CollisionState.STILL_COLLIDING:
-            interpolated_goal_position = self.compute_close_smart_goal_position()
+            interpolated_goal_position = self.compute_fake_error_goal_position()
+            self.safe_computed_goal_position = interpolated_goal_position
 
         elif collision_state == CollisionState.LEAVING_COLLISION:
-            self.set_pid(gripper, p=P_DIRECT_CONTROL, i=0.0, d=0.0)
+            self.set_pid(p=P_DIRECT_CONTROL, i=0.0, d=0.0)
             interpolated_goal_position = self.compute_close_smart_goal_position()
+            self.safe_computed_goal_position = new_user_requested_goal_position
 
         self.interpolated_goal_position.append(interpolated_goal_position)
         self.error.append(interpolated_goal_position - new_present_position)
+
         self.in_collision.append(collision_state in (CollisionState.ENTERING_COLLISION, CollisionState.STILL_COLLIDING))
 
-        self.elapsed_dts_since_change_of_state += 1
+        self.elapsed_dts_since_change_of_direction += 1
         self.elapsed_dts_since_collision += 1
 
     def check_collision_state(self) -> CollisionState:
         if not hasattr(self, '_hidden_collision_state'):
             self._hidden_collision_state = CollisionState.NO_COLLISION
 
-        if self.entering_collision():
+        if not self.in_collision[-1] and self.entering_collision():
             self._hidden_collision_state = CollisionState.STILL_COLLIDING
             return CollisionState.ENTERING_COLLISION
-            
-        if self.leaving_collision():
+
+        if self.in_collision[-1] and self.leaving_collision():
             self._hidden_collision_state = CollisionState.NO_COLLISION
+            self.elapsed_dts_since_collision = 0
             return CollisionState.LEAVING_COLLISION
 
         return self._hidden_collision_state
@@ -129,10 +141,14 @@ class GripperState:
             return False
 
         filtered_error = np.mean(self.error)
-        return filtered_error > MAX_ERROR if self.is_direct else filtered_error < -MAX_ERROR
+        return (
+            (filtered_error > MAX_ERROR and self.user_requested_goal_position[-1] > self.present_position[-1])
+            if self.is_direct
+            else (filtered_error < -MAX_ERROR and self.user_requested_goal_position[-1] < self.present_position[-1])
+        )
 
     def leaving_collision(self) -> bool:
-        last_req_goal_pos = self.requested_goal_position[-1]
+        last_req_goal_pos = self.user_requested_goal_position[-1]
         last_interp_goal_pos = self.interpolated_goal_position[-1]
 
         # This condition is triggered by a direct user command to release our grasp
@@ -147,26 +163,30 @@ class GripperState:
         #   - because it was a false detection in the first place
         moving_again = (
             self.elapsed_dts_since_collision > self.present_position.maxlen and
-            abs(self.present_position[0] - self.present_position[-1]) > MIN_MOVING_DIST
+            ((self.present_position[0] - self.present_position[-1]) < -MIN_MOVING_DIST
+                if self.is_direct
+                else (self.present_position[0] - self.present_position[-1]) > MIN_MOVING_DIST)
         )
+
+        # print(f'user: {user_request_to_release}, moving_again: {moving_again}, evol: {abs(self.present_position[0] - self.present_position[-1])}')
 
         return user_request_to_release or moving_again
 
     def has_changed_direction(self, new_goal_pos: float) -> bool:
         present_position = self.present_position[-1]
-        last_goal_pos = self.requested_goal_position[-1]
+        last_goal_pos = self.user_requested_goal_position[-1]
 
         return np.sign(last_goal_pos - present_position) != np.sign(new_goal_pos - present_position)
 
     def compute_close_smart_goal_position(self) -> float:
-        last_req_goal_pos = self.requested_goal_position[-1]
+        last_req_goal_pos = self.user_requested_goal_position[-1]
         goal_offset = INC_PER_DT * np.sign(last_req_goal_pos - self.present_position[-1])
 
         next_goal_pos = self.interpolated_goal_position[-1] + goal_offset
 
         return (
             min(next_goal_pos, last_req_goal_pos)
-            if self.is_direct else
+            if goal_offset > 0 else
             max(next_goal_pos, last_req_goal_pos)
         )
         
@@ -176,6 +196,9 @@ class GripperState:
             model_offset = -model_offset
 
         return model_offset + self.present_position[-1]
+
+    def set_pid(self, p, i, d) -> None:
+        self.pid = p, i, d
 
 
 class GrippersController(Node):
@@ -224,27 +247,43 @@ class GrippersController(Node):
         )
         self.logger.info(f'Subscribe to "{self.grippers_sub.topic_name}".')
 
-        # TODO: introspection pour savoir quel gripper ?
+        self.grippers: Dict[str, Dict[str, float]] = {}
 
-        self.grippers = {
-            'l_gripper': GripperState(is_direct=False),
-            'r_gripper': GripperState(is_direct=True),
+        self.first_init = Event()
+        self.wait_for_setup()
+
+        self.gripper_states = {
+            name: GripperState(
+                name, is_direct=name.startswith('r'),
+                present_position=value['present_position'], user_requested_goal_position=value['user_requested_goal_position'],
+            )
+            for name, value in self.grippers.items()
+        }
+
+        self.last_grippers_pid = {
+            name: state.pid
+            for name, state in self.gripper_states.items()
         }
 
         def gripper_state_update_thread():
-            for gripper in self.grippers.values():
-                self.set_pid(gripper, p=P_DIRECT_CONTROL, i=0.0, d=0.0)
+            self.publish_goals()
+            self.publish_pids()
 
             while rclpy.ok():
                 self.gripper_state_update()
                 time.sleep(DT)
 
-        # TODO: Use ROS timer?
         self.gripper_state_thread = Thread(target=gripper_state_update_thread)
         self.gripper_state_thread.daemon = True
         self.gripper_state_thread.start()
 
         self.logger.info('Node ready!')
+
+    def wait_for_setup(self):
+        while True:
+            rclpy.spin_once(self)
+            if self.first_init.wait(timeout=0.1):
+                break
 
     def grippers_callback(self, msg: Gripper):
         """Get latest Gripper msg from /grippers."""
@@ -252,27 +291,35 @@ class GrippersController(Node):
         for name, goal_position in zip(msg.name, msg.goal_position):
             if name not in self.grippers:
                 continue
-            gripper = self.grippers[name]
-            gripper.previous_requested_goal_position = gripper.requested_goal_position
-            gripper.requested_goal_position = goal_position
+            self.grippers[name]['user_requested_goal_position'] = goal_position
 
     def joint_states_callback(self, msg: JointState):
         """Get latest JointState from /joint_states."""
+        if not self.first_init.is_set():
+            self.setup_grippers(msg)
+            self.first_init.set()
+
         for name, position in zip(msg.name, msg.position):
             if name not in self.grippers:
                 continue
 
-            gripper = self.grippers[name]
-            gripper._previous_present_position = gripper.present_position
-            gripper.present_position = position
-            gripper.error = gripper.goal_position - gripper.present_position
+            self.grippers[name]['present_position'] = position
 
-            gripper.present_position_evolution = gripper.present_position
+    def setup_grippers(self, msg: JointState):
+        for name, position in zip(msg.name, msg.position):
+            if name.endswith('gripper'):
+                self.grippers[name] = {
+                    'present_position': position,
+                    'user_requested_goal_position': position,
+                }
 
     def gripper_state_update(self):
         """Update grippers state machine."""
-        for gripper_state in self.grippers.values():
-            gripper_state.update()
+        for name, gripper_state in self.gripper_states.items():
+            gripper_state.update(
+                new_present_position=self.grippers[name]['present_position'],
+                new_user_requested_goal_position=self.grippers[name]['user_requested_goal_position'],
+            )
 
         self.publish_goals()
         self.publish_pids()
@@ -281,31 +328,31 @@ class GrippersController(Node):
         """Publish new /goal_states for grippers."""
         goals = JointState()
 
-        for gripper in self.grippers.values():
-            goals.name.append(gripper.name)
+        for name, gripper_state in self.gripper_states.items():
+            goals.name.append(name)
 
-            if(gripper.in_collision):
-                goals.position.append(gripper.goal_position)
-            else:
-                goals.position.append(gripper.requested_goal_position)
+            goals.position.append(gripper_state.safe_computed_goal_position)
 
         if goals.name:
             self.logger.debug(f'Publish "{goals}" to "{self.joint_goals_publisher.topic_name}".')
             self.joint_goals_publisher.publish(goals)
 
-    def set_pid(self, gripper, p, i, d):
-        """Set gripper PID to new value."""
-        gains = PidGains()
-        gains.p = p
-        gains.i = i
-        gains.d = d
+    def publish_pids(self):
+        """Publish new PID requests for grippers."""
+        for name, gripper_state in self.gripper_states.items():
+            if gripper_state.pid != self.last_grippers_pid[name]:
+                gains = PidGains()
+                gains.p, gains.i, gains.d = gripper_state.pid
 
-        pid = SetJointPidGains.Request()
-        pid.name.append(gripper.name)
-        pid.pid_gain.append(gains)
+                pid = SetJointPidGains.Request()
+                pid.name.append(name)
+                pid.pid_gain.append(gains)
 
-        resp = self.pid_gains_client.call(pid)
-        self.logger.info(f'PID gains {(p, i, d)} set for "{gripper.name}" with resp "{resp.success}".')
+                resp = self.pid_gains_client.call(pid)
+                self.logger.info(f'PID gains {(gains.p, gains.i, gains.d)} set for "{name}" with resp "{resp.success}".')
+
+                if resp:
+                    self.last_grippers_pid[name] = gripper_state.pid
 
 
 def main(args=None):
